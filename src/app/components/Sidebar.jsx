c8f9291a18582bf1cb8e478f259866326f2e190f
Sachin Bhujel <90704616+sachinbhujel@users.noreply.github.com>
2025-09-27 10:28:39 +0000

    Made some changes in sidebar

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -66,7 +66,7 @@ function Sidebar({ isVisible, setIsVisible }) {
                 } border ${sidebarWidth ? "w-max" : "w-[250px]"
                 } overflow-y-auto custom-scrollbar`}
         >
-            <div className="flex flex-col gap-10 justify-between h-full">
+            <div className="flex flex-col justify-between h-full">
                 <div>
                     <div
                         className={`items-center gap-20 justify-between ${sidebarWidth ? "flex-col" : "flex"
@@ -272,6 +272,21 @@ function Sidebar({ isVisible, setIsVisible }) {
                 </div>
 
                 <div className="flex flex-col gap-2">
+                    <a href="https://github.com/sachinbhujel/DiffyAI">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className={`cursor-pointer lucide lucide-github-icon lucide-github w-max flex ${sidebarWidth ? "m-auto" : "m-left"} `}>
+                            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                            <path d="M9 18c-4.51 2-5-2-7-2" />
+                        </svg>
+                    </a>
                     <div
                         className={`w-max flex ${sidebarWidth ? "m-auto" : "m-left"
                             }`}
