5c7aad3d53bb16a3dcef0fb19aff202e7f1691c8
Sachin Bhujel <90704616+sachinbhujel@users.noreply.github.com>
2025-09-29 16:57:28 +0000

    Del Button Added

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 // Get all keys in the store | Get all values in the store
-import { keys, set, values } from "idb-keyval";
+import { keys, set, values, del } from "idb-keyval";
 
 function Sidebar({ isVisible, setIsVisible }) {
     const [sidebarWidth, setSidebarWidth] = useState(false);
@@ -65,7 +65,7 @@ function Sidebar({ isVisible, setIsVisible }) {
         <div
             className={`rounded-md border-primary border-2 bg-background z-10 flex flex-col p-2 ${isVisible ? "fixed sm:static h-[calc(100vh-16px)]" : ""
                 } border ${sidebarWidth ? "w-max" : "w-[250px]"
-                }`}
+                } overflow-y-auto`}
         >
             <div className="flex flex-col justify-between h-full">
                 <div>
@@ -255,19 +255,20 @@ function Sidebar({ isVisible, setIsVisible }) {
                                                             } px-2 py-2 flex items-center justify-between text-sm font-medium group`}
                                                     >
                                                         {chat}
-                                                        <button className="cursor-pointer opacity-0 group-hover:opacity-100">
+                                                        <button className="cursor-pointer opacity-0 group-hover:opacity-100" onClick={() => del(`${id[index]}`)}>
                                                             <svg xmlns="http://www.w3.org/2000/svg"
-                                                                width="22"
-                                                                height="22"
+                                                                width="18"
+                                                                height="18"
                                                                 viewBox="0 0 24 24"
                                                                 fill="none"
                                                                 stroke="currentColor"
                                                                 strokeWidth="2"
                                                                 strokeLinecap="round"
                                                                 strokeLinejoin="round"
-                                                                className="lucide lucide-ellipsis-icon lucide-ellipsis">
-                                                                <circle cx="12" cy="12" r="1" /><circle cx="19" cy="12" r="1" /><
-                                                                    circle cx="5" cy="12" r="1" />
+                                                                className="lucide lucide-trash-icon lucide-trash">
+                                                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                                                                <path d="M3 6h18" />
+                                                                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
                                                             </svg>
                                                         </button>
                                                     </Link>
